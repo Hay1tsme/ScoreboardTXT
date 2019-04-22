dd860db136deb2c14fe894f5d844e2a39961c2ea
Kevin <pitok236@gmail.com>
2019-04-22 14:18:13 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ScoreboardTXT
---
+++ src/me/hay1ts/sbtxt/Boot.java
@@ -0,0 +1,10 @@
+package me.hay1ts.sbtxt;
+
+/**
+ * Main class, handles creating the initial GUI as well as initialization
+ */
+public class Boot {
+    public static void main(String args[]) {
+
+    }
+}
